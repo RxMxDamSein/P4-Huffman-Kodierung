d1490c6442e288de5a909046ce17ab5042e085fa
Arkanipro <Arkanipro@gmail.com>
2021-01-15 23:06:27 +0100

    now using codes Array
    offene Frage muss man wirklich alle teile im Array mit 0 initialisieren?!

--- src/huffman.java
+++ src/huffman.java
@@ -29,33 +29,32 @@ class Huffman {
 	// Prüfen, ob ein Text mit dem aktuell erstellten Huffman-Code kodiert werden kann, ob also alle Zeichen einen Präfix-Code besitzen. Wenn ja, return true, wenn nein, return false. 
 	// Prüfen, ob ein Text mit dem aktuell erstellten Huffman-Code kodiert werden kann, ob also alle Zeichen einen Präfix-Code besitzen. Wenn ja, return true, wenn nein, return false.
 	public boolean canEncode(String text){
-		zeichen=new ArrayList<>();
-		getZeichen(root);
+		if(codes==null ){
+			System.out.println("Es gibt noch keine Präfixcodes!");
+			return false;
+		}else if(text==null){
+			System.out.println("Der übergebenen Text ist null!");
+			return false;
+		}
+
+
 		for(char c:text.toCharArray()){
-			if(!zeichen.contains(c))
+			if(codes[c]==null)
 				return false;
 		}
 		return true;
 	}
 
-	private ArrayList<Character> zeichen;
-	private void getZeichen(HNode n){
-		if(n.leftChild!=null){
-			getZeichen(n.leftChild);
-			getZeichen(n.rightChild);
-		}else{
-			Character c=n.chars.toCharArray()[0];
-			if (!zeichen.contains(c)){
-				zeichen.add(c);
-			}
-		}
-	}
 
 	// Vor dem eigentlichen Algorithmus kann mit dieser Funktion die Häufigkeit der einzelnen Zeichen aus dem übergebenen Text errechnet werden.
 	// Hierzu kann die Anzahl des Vorkommens eines Zeichens berechnet werden und in einem Array gespeichert werden.
 	// Für jedes Zeichen c enthält das Array f an Stelle c die Häufigkeit (also f['a'] ist die Häufigkeit von a im Text. Kommt das Zeichen nicht vor, ist die Häufigkeit 0.)
 	// Zur Erinnerung: ein char kann wie eine Ganzzahl verwendet werden, daher funktioniert f[c] für jedes char c.
 	public Integer[] calculateFrequencies(String text){
+		if(text==null){
+			System.out.println("Der übergebenen Text ist null!");
+			return null;
+		}
 		Integer[] f = new Integer[256];
 		for(char c:text.toCharArray()){
 			if(c>255 || c<0)
@@ -71,12 +70,16 @@ class Huffman {
 	// frequencies enthält die Häufigkeiten (siehe calculateFrequencies). Häufigkeit von 0 bedeutet, das entsprechende Zeichen ist nicht im Text vorhanden und wir brauchen keinen Präfixcode dafür.
 	// Die Funktion setzt den Knoten root auf den Wurzelknoten des PräfixCode-Baums und gibt diesen Wurzelknoten außerdem zurück
 	public HNode constructPrefixCode(Integer[] frequencies){
+		if(frequencies==null){
+			System.out.println("Das übergebenen frequencies-Objekt ist null!");
+			return null;
+		}
 		BinHeap<Integer,HNode> binHeap=new BinHeap<>();
-		//ArrayList<BinHeap.Entry<Integer,String>> entries=new ArrayList<>();
+		//ArrayList<BinHeap.Entry<Integer,HNode>> entries=new ArrayList<>();
+
 		for(int i=0;i<frequencies.length;i++){
-			if(frequencies[i]==null)
+			if(frequencies[i]==null || frequencies[i]==0)
 				continue;
-			//entries.add(binHeap.insert(frequencies[i],""+(char)i));
 			HNode hNode=new HNode();
 			hNode.chars=""+(char)i;
 			binHeap.insert(frequencies[i],hNode);
@@ -97,7 +100,19 @@ class Huffman {
 			binHeap.insert(e1.prio()+e2.prio(),hNode);
 		}
 		root=binHeap.extractMin().data();
+		codes=new String[frequencies.length];
+		makeCodes(root,"");
 		return root;
+	}//nichts zu bescheren
+
+	private void makeCodes(HNode n,String code){
+		if(n.leftChild!=null){
+			makeCodes(n.leftChild,code+"0");
+			makeCodes(n.rightChild,code+"1");
+		}else{
+			//System.out.println(n.chars.charAt(0)+": "+code);
+			codes[n.chars.charAt(0)]=code;
+		}
 	}
 
 	// Kodierung einer Zeichenkette text (Skript S.108)
@@ -107,6 +122,10 @@ class Huffman {
 	// Erster Parameter: Zu kodierender Text
 	// Zweiter Parameter zeigt an, ob ein neuer Präfixcode erzeugt werden soll (true) oder mit dem aktuellen Präfixcode gearbeitet werden soll (false)
 	public String encode(String text, boolean newPrefixCode){
+		if(text==null){
+			System.out.println("Der übergebenen Text ist null!");
+			return null;
+		}
 		if(newPrefixCode){
 			constructPrefixCode(calculateFrequencies(text));
 		}else{
@@ -133,12 +152,19 @@ class Huffman {
 	// Dekodierung eines Huffman-Kodierten Textes. (Skipt S.107)
 	// Die Ergebnis-Zeichenkette ist der ursprüngliche Text vor der Huffman-Kodierung
 	public String decode(String huffmanEncoded){
-		return (root==null)?null:decode(huffmanEncoded,root);
+		return decode(huffmanEncoded,root);
 	}
 
 	// Dekodierung eines Huffman-Kodierten Textes mithilfe des übergebenen Präfix-Codebaums. (Skipt S.107) Der aktuelle Baum soll dabei nicht überschrieben werden.
 	// Die Ergebnis-Zeichenkette ist der ursprüngliche Text vor der Huffman-Kodierung
 	public String decode(String huffmanEncoded, HNode rootNode){
+		if(huffmanEncoded==null){
+			System.out.println("Der übergebenen Text ist null!");
+			return null;
+		}else if(rootNode==null){
+			System.out.println("Es gibt keine root Node!");
+			return null;
+		}
 		String result = "";
 		HNode cNode=rootNode;
 		for(char c:huffmanEncoded.toCharArray()){
@@ -157,7 +183,7 @@ class Huffman {
 		}
 		//System.out.println();
 		return result;
-	}
+	}//wie im skribt
 
 	// Präfixcodes ausgeben
 // Reihenfolge: preOrder, also WLR, zuerst Wurzel,
